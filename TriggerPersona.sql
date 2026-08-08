06b9d0bfc58ec7808e59abd01190ff9b028ec99f
criv <criv200114@gmail.com>
2026-08-08 11:20:55 -0600

    Paciente
    Se toma los valores antiguos y muestra los nuevos, se anadio los triggers de paciente

--- TriggerPersona.sql
+++ TriggerPersona.sql
@@ -2,14 +2,14 @@
 
  /* 1. tipo de tabla 'TVP'     disque para permitir poner varias cosas en lo de valor nuevo/viejo pero con lo de python       drop type TipoAuditoria;*/ 
 
-Create type TipoAuditoria as table
+create type TipoAuditoria as table
 (
     ID_USUARIO         int           null,
     TABLA_AFECTADA     Varchar(50)   not null,
     REGISTRO_AFECTADO  int           not null,
     ACCION             Varchar(10)   not null,
-    VALOR_ANTERIOR     Varchar(50)  null,
-    VALOR_NUEVO        Varchar(50)  null
+    VALOR_ANTERIOR     Varchar(max)  null,
+    VALOR_NUEVO        Varchar(max)  null
 );
 go
 
@@ -41,42 +41,78 @@ go
 /* 3. Triggers
 
 
--- ---------------- Persona ---------------- */ 
-Alter Trigger TR_Persona_Insert on PERSONA after insert as
+------------------ Persona ---------------- */ 
+Create Trigger TR_Persona_Insert on PERSONA after insert as
 begin
 
     Declare @Aud TipoAuditoria;
     Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
-    Select 1 , 'PERSONA', i.ID_PERSONA, 'INSERT', NULL, 'INSERT' /*  CAST(SESSION_CONTEXT(N'ID_USUARIO') as int) */
+    Select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int) , 'PERSONA', i.ID_PERSONA, 'INSERT', NULL, (select i.* for json path, WITHOUT_ARRAY_WRAPPER)
     From inserted i;
     exec SP_RegistrarAuditoria @Registros = @Aud;
 end;
 go
 
-
+Select * from PERSONA
 select * from USUARIO
 select * from AUDITORIA
 go
 
-Alter Trigger TR_Persona_Update on PERSONA after update as
+Create Trigger TR_Persona_Update on PERSONA after update as
 begin
     Declare @Aud TipoAuditoria;
     Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
-    select 2, 'PERSONA', i.ID_PERSONA, 'update', 'Valor Anterior', 'Valor nuevo' /*  CAST(SESSION_CONTEXT(N'ID_USUARIO') as int) */
-    From inserted i;
+    select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int), 'PERSONA', i.ID_PERSONA, 'UPDATE', (select d.* for json path, WITHOUT_ARRAY_WRAPPER), (select i.* for json path, WITHOUT_ARRAY_WRAPPER) 
+    From inserted i
+    Inner join deleted d on d.ID_PERSONA = i.ID_PERSONA;
     Exec SP_RegistrarAuditoria @Registros = @Aud;
 end;
 go
 
-alter Trigger TR_Persona_Delete on PERSONA after delete as
+Create Trigger TR_Persona_Delete on PERSONA after delete as
 Begin
 
     Declare @Aud TipoAuditoria;
     Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
-    select 3, 'PERSONA', d.ID_PERSONA, 'delete', 'Registro existente', NULL /*  CAST(SESSION_CONTEXT(N'ID_USUARIO') as int) */
+    select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int), 'PERSONA', d.ID_PERSONA, 'DELETE', (select d.* for json path, WITHOUT_ARRAY_WRAPPER), NULL 
     from deleted d;
     exec SP_RegistrarAuditoria @Registros = @Aud;
 end;
 go
 
 delete PERSONA where ID_PERSONA = 34
+
+/*---------------- Paciente ----------------*/
+
+
+Create Trigger TR_Paciente_Insert on PACIENTE after insert as
+begin
+    Declare @Aud TipoAuditoria;
+    Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
+    Select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int) , 'PACIENTE', i.ID_PACIENTE, 'INSERT', NULL, (select i.* for json path, WITHOUT_ARRAY_WRAPPER)
+    From inserted i;
+    exec SP_RegistrarAuditoria @Registros = @Aud;
+end;
+GO
+
+Create Trigger TR_Paciente_Update on PACIENTE after update as
+begin
+    Declare @Aud TipoAuditoria;
+    Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
+    select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int), 'PACIENTE', i.ID_PACIENTE, 'UPDATE', (select d.* for json path, WITHOUT_ARRAY_WRAPPER), (select i.* for json path, WITHOUT_ARRAY_WRAPPER) 
+    From inserted i
+    Inner join deleted d on d.ID_PACIENTE = i.ID_PACIENTE;
+    Exec SP_RegistrarAuditoria @Registros = @Aud;
+end;
+GO
+
+
+Create Trigger TR_Paciente_Delete on PACIENTE after delete as
+Begin
+    Declare @Aud TipoAuditoria;
+    Insert into @Aud (ID_USUARIO, TABLA_AFECTADA, REGISTRO_AFECTADO, ACCION, VALOR_ANTERIOR, VALOR_NUEVO)
+    select CAST(SESSION_CONTEXT(N'ID_USUARIO') as int), 'PACIENTE', d.ID_PACIENTE, 'DELETE', (select d.* for json path, WITHOUT_ARRAY_WRAPPER), NULL 
+    from deleted d;
+    exec SP_RegistrarAuditoria @Registros = @Aud;
+end;
+GO
